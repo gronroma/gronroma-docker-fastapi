cffaf3691c3ce148468a60341750d77d0e0b1f7d
gronroma <maxrgronroos@gmail.com>
2026-04-29 13:44:45 +0300

    add ip api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return { "msg": "Hello local docker!", "v": "0.5" }
+
+@app.get("/api/ip")
+def ip(request: Request):
+    return {"ip": request.client.host}
 
 @app.get("/hello")
 def hello():
